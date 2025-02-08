a24d31fd06aea090e39f19e98351e310dcd31393
Usama Thebo <usamaathebo@gmail.com>
2025-02-09 01:43:34 +0500

    sss

--- tasks/models.py
+++ tasks/models.py
@@ -14,6 +14,7 @@ class Add_task(models.Model):
     duration = models.DurationField()
     status = models.CharField(max_length=10, choices=Status_choice, default='Pending')
     created_on = models.DateTimeField(default=timezone.now)  # Manually set default
+    session_id = models.CharField(max_length=100, blank=True, null=True)  # 🔹 Add this field
 
     def __str__(self):
         return f"{self.task_name}  ({self.status})"
