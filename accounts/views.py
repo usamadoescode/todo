c65854b7c5d4e00a044b8bd188db6bd0812b638b
Usama Thebo <usamaathebo@gmail.com>
2025-02-10 00:23:39 +0500

    logout added

--- accounts/views.py
+++ accounts/views.py
@@ -31,4 +31,4 @@ def user_login(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return redirect("accounts:login")  # ✅ Redirect correctly
+    return redirect("accounts:user_login")  # ✅ Redirect correctly
