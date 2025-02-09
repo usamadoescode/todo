e0728add45ad4258bb53abb4dea8653abed2573b
Usama Thebo <usamaathebo@gmail.com>
2025-02-09 22:44:20 +0500

    u

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ app_name = "tasks"
 urlpatterns = [
     
     path("", views.user_login, name="user_login"),  # ✅ Default page is login
-    path("register/", views.register, name="register"),
+    path("register/", views.register, name="register"),  # ✅ Register route
     path("logout/", views.user_logout, name="logout"),
 ]
